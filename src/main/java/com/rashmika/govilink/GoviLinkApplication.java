669b835b80bcd836dd1d5803db694514bf3ef263
rashmika <rashmika6145@gmial.com>
2025-12-18 08:23:27 +0530

    init commit

---
+++ src/main/java/com/rashmika/govilink/GoviLinkApplication.java
@@ -0,0 +1,13 @@
+package com.rashmika.govilink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoviLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoviLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rashmika/govilink/GoviLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashmika.govilink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoviLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
